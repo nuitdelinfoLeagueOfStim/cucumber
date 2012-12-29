b4b3363b9181448289d214dd77841b0ff1f062f0
matthieu.drula@stim.re <matthieu.drula@stim.re>
2012-12-29 21:57:33 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogCucumber::Application.config.secret_token = '70aa14e646549cc4413cf3cc8aaebf19eeb37c7911dfc40880f3ec62a49b2f52c9cb9183c95729c5c47430185111a61099effdf1a48b8a7841d2be8b7a5b29a4'
